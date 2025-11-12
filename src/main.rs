d4d9072876ac359ee6e7a1559c2607b3094ecce1
Nyx <alexanderbeyer112@gmail.com>
2025-11-12 10:06:29 +0100

    add a file read sytem add user imput start to add a wirte sytem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::fs::{self, OpenOptions};
+use std::io::{Write, stdin, stdout};
+
 fn main() {
-    println!("Hello, world!");
+    let _ = OpenOptions::new()
+        .create_new(true)
+        .write(true)
+        .open("todo_list");
+
+    file_reader();
+
+    let input = user_input();
+
+    file_wirter(input);
+    file_reader();
 }
+
+fn user_input() -> String {
+    let mut _input = String::new();
+    println!("Please enter a Todoo");
+    let _ = stdout().flush();
+    stdin().read_line(&mut _input).unwrap().to_string()
+}
+
+fn file_reader() {
+    let todo_list = fs::read_to_string("todo_list").expect("faild by read file");
+    for s in todo_list.chars() {
+        print!("{s}");
+    }
+}
+
+fn file_wirter(input: String) {
+    let _ = fs::write("todo_list", input);
+}
+// make file to string and add the todo at the and withe space and put it back in the file
